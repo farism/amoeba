a65b4beb20de144583eab630a7bfebce7fdd792f
Faris Mustafa <faris.mustafa@procore.com>
2025-03-21 15:41:58 -0700

    platform

--- build.ts
+++ build.ts
@@ -1,14 +1,16 @@
+import os from "node:os";
 import { mkdir } from "node:fs/promises";
-import { $ } from 'bun';
+import { $ } from "bun";
 
-const extension = Bun.argv[2];
+const PLATFORM = os.platform();
+const ARCH = os.arch();
+const DEBUG = "debug";
+const RELEASE = "release";
 
-$.nothrow()
+$.nothrow();
 
-for(const config of ['debug', 'release']) {
-    for(const platform of ['windows', 'linux', 'macos']) {
-        await mkdir(`libs/${config}/${platform}`, {recursive: true})
-    }
+for (const config of [DEBUG, RELEASE]) {
+  await mkdir(`libs/${config}/${PLATFORM}_${ARCH}`, { recursive: true });
 }
 
 await $`
@@ -18,16 +20,14 @@ cp amoeba/amoeba.h amoeba/amoeba.c
 cmake -S . -B amoeba/build -DCMAKE_BUILD_TYPE=Debug
 cmake --build amoeba/build --config Debug
 
-cp amoeba/build/Debug/amoeba.lib libs/debug/windows
-cp amoeba/build/Debug/amoeba.pdb libs/debug/windows
-cp amoeba/build/amoeba.a libs/debug/linux
-cp amoeba/build/amoeba.a libs/debug/mac
+cp amoeba/build/Debug/amoeba.lib libs/${DEBUG}/${PLATFORM}_${ARCH}
+cp amoeba/build/Debug/amoeba.pdb libs/${DEBUG}/${PLATFORM}_${ARCH}
+cp amoeba/build/amoeba.a libs/${DEBUG}/${PLATFORM}_${ARCH}
 
-cmake -S . -B amoeba/build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B amoeba/build -DCMAKE_BUILD_TYPE=Release 
 cmake --build amoeba/build --config Release
 
-cp amoeba/build/Release/amoeba.lib libs/release/windows/
-cp amoeba/build/amoeba.a libs/release/linux/
-cp amoeba/build/amoeba.a libs/release/mac
+cp amoeba/build/Release/amoeba.lib libs/${RELEASE}/${PLATFORM}_${ARCH}
+cp amoeba/build/amoeba.a libs/${RELEASE}/${PLATFORM}_${ARCH}
 
-`
+`;
